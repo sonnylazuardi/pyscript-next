953f2bc32399a7b85b4109aced163ce113981931
sonnylazuardi <sonnylazuardi@gmail.com>
2022-05-02 00:26:01 +0800

    1 file modified

--- pages/api/pyscript.js
+++ pages/api/pyscript.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+  const body = JSON.parse(req.body);
+
   res.status(200).send(`
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +16,7 @@ export default (req, res) => {
 </head>
 <body>
 <py-script>
-${req.body.code}
+${body.code}
 </py-script>
 </body>
 </html>
